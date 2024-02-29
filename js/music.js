94431845738cc6d9eda1053177e9cdf0b37c3e97
syan0o0 <1095412093@qq.com>
2024-02-29 22:44:09 +0800

    Site updated: 2024-02-29 22:44:09

---
+++ js/music.js
@@ -0,0 +1,13 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+	autoplay: true, //自动播放
+    audio: [
+	{
+        name: 'Happy Birthday',
+        artist: '',
+        url: 'https://m10.music.126.net/20240229223005/7d8262715c0e9deb1583f1105263cac9/yyaac/obj/wonDkMOGw6XDiTHCmMOi/1904550975/d6d3/d0aa/ba19/fe77247cdc922b0a7ce9c093c4de168e.m4a',
+        cover: '',
+    }, 
+	]
+});
